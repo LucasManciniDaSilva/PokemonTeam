867a8dab7e688f056ac521a583b5946248f9a303
LucasManciniDaSilva <lucasmancini1997@gmail.com>
2021-09-06 19:56:12 -0300

    Create more tests with test containers

--- src/test/java/com/example/pokemondreamteam/testContainer/PokemonDreamTeamTests.java
+++ src/test/java/com/example/pokemondreamteam/testContainer/PokemonDreamTeamTests.java
@@ -1,6 +1,7 @@
 package com.example.pokemondreamteam.testContainer;
 
 import com.example.pokemondreamteam.interfaces.json.Team.Team;
+import com.example.pokemondreamteam.interfaces.json.Team.TeamPatch;
 import com.example.pokemondreamteam.interfaces.json.Team.TeamPost;
 import com.github.javafaker.Faker;
 import org.junit.Assert;
@@ -48,11 +49,26 @@ public class PokemonDreamTeamTests {
     return teamPost;
   }
 
+  private TeamPatch updateTeam() {
+
+    TeamPatch teamPatch = new TeamPatch();
+    teamPatch.setFirstPokemon("Gible");
+    teamPatch.setSecondPokemon("Charmander");
+    teamPatch.setThirdPokemon("Mudkip");
+    teamPatch.setFourthPokemon("Bagon");
+    teamPatch.setFifthPokemon("Quilava");
+    teamPatch.setLastPokemon("Dratini");
+
+    return teamPatch;
+  }
+
+
+
   @Test
   public void postTeamWithSuccess() {
     TeamPost teamPost = createNewTeam();
 
-    ResponseEntity<Team> postResponse =
+    ResponseEntity postResponse =
         restTemplate.postForEntity(getRootUrl() + "/team", teamPost, null);
 
     logger.info("Pokemon Team Post Request"+ teamPost.toString());
@@ -62,6 +78,22 @@ public class PokemonDreamTeamTests {
     logger.info("Pokemon Team Name:" + teamName);
 
     Assert.assertEquals(teamName, teamPost.getTeamName());
+
+    HttpHeaders headers = new HttpHeaders();
+    HttpEntity<String> entity = new HttpEntity<>(null, headers);
+
+    ResponseEntity deleteTeam =
+            restTemplate.exchange(
+                    getRootUrl()
+                            + "/team/"
+                            + Objects.requireNonNull(postResponse.getHeaders()).getFirst("teamName"),
+                    HttpMethod.DELETE,
+                    entity,
+                    (Class<Team>) null);
+
+    logger.info("Delete Pokemon Team status code returned: " + deleteTeam.getStatusCode());
+
+    Assert.assertEquals(deleteTeam.getStatusCode(), HttpStatus.NO_CONTENT);
   }
 
   @Test
@@ -69,7 +101,7 @@ public class PokemonDreamTeamTests {
     TeamPost teamPost = createNewTeam();
     teamPost.setTeamName("lucio.rodriguez");
 
-    ResponseEntity<Team> postResponse =
+    ResponseEntity postResponse =
         restTemplate.postForEntity(getRootUrl() + "/team", teamPost, null);
 
     logger.info("Pokemon Team Post Request"+ teamPost.toString());
@@ -86,7 +118,7 @@ public class PokemonDreamTeamTests {
     TeamPost teamPost = createNewTeam();
     teamPost.setFirstPokemon("Mewthree");
 
-    ResponseEntity<Team> postResponse =
+    ResponseEntity postResponse =
         restTemplate.postForEntity(getRootUrl() + "/team", teamPost, null);
 
     logger.info("Pokemon Team Post Request"+ teamPost.toString());
@@ -103,7 +135,7 @@ public class PokemonDreamTeamTests {
     TeamPost teamPost = createNewTeam();
     teamPost.setFirstPokemon(null);
 
-    ResponseEntity<Team> postResponse =
+    ResponseEntity postResponse =
         restTemplate.postForEntity(getRootUrl() + "/team", teamPost, null);
 
     logger.info("Pokemon Team Post Request"+ teamPost.toString());
@@ -120,7 +152,7 @@ public class PokemonDreamTeamTests {
     TeamPost teamPost = createNewTeam();
     teamPost.setSecondPokemon(null);
 
-    ResponseEntity<Team> postResponse =
+    ResponseEntity postResponse =
         restTemplate.postForEntity(getRootUrl() + "/team", teamPost, null);
 
     logger.info("Pokemon Team Post Request"+ teamPost.toString());
@@ -137,7 +169,7 @@ public class PokemonDreamTeamTests {
     TeamPost teamPost = createNewTeam();
     teamPost.setThirdPokemon(null);
 
-    ResponseEntity<Team> postResponse =
+    ResponseEntity postResponse =
         restTemplate.postForEntity(getRootUrl() + "/team", teamPost, null);
 
     logger.info("Pokemon Team Post Request"+ teamPost.toString());
@@ -154,7 +186,7 @@ public class PokemonDreamTeamTests {
     TeamPost teamPost = createNewTeam();
     teamPost.setFourthPokemon(null);
 
-    ResponseEntity<Team> postResponse =
+    ResponseEntity postResponse =
         restTemplate.postForEntity(getRootUrl() + "/team", teamPost, null);
 
     logger.info("Pokemon Team Post Request"+ teamPost.toString());
@@ -171,7 +203,7 @@ public class PokemonDreamTeamTests {
     TeamPost teamPost = createNewTeam();
     teamPost.setFifthPokemon(null);
 
-    ResponseEntity<Team> postResponse =
+    ResponseEntity postResponse =
         restTemplate.postForEntity(getRootUrl() + "/team", teamPost, null);
 
     logger.info("Pokemon Team Post Request"+ teamPost.toString());
@@ -188,7 +220,7 @@ public class PokemonDreamTeamTests {
     TeamPost teamPost = createNewTeam();
     teamPost.setLastPokemon(null);
 
-    ResponseEntity<Team> postResponse =
+    ResponseEntity postResponse =
         restTemplate.postForEntity(getRootUrl() + "/team", teamPost, null);
 
     logger.info("Pokemon Team Post Request"+ teamPost.toString());
@@ -204,34 +236,34 @@ public class PokemonDreamTeamTests {
   public void getPokemonTeamWithSuccess() {
     TeamPost teamPost = createNewTeam();
 
-    ResponseEntity<Team> postResponse =
-        restTemplate.postForEntity(getRootUrl() + "/team", teamPost, null);
+    ResponseEntity postResponse =
+            restTemplate.postForEntity(getRootUrl() + "/team", teamPost, null);
 
     HttpHeaders headers = new HttpHeaders();
     HttpEntity<String> entity = new HttpEntity<>(null, headers);
     ResponseEntity<Team> response =
-        restTemplate.exchange(
-            getRootUrl()
-                + "/team/"
-                + Objects.requireNonNull(postResponse.getHeaders()).getFirst("teamName"),
-            HttpMethod.GET,
-            entity,
-            Team.class);
+            restTemplate.exchange(
+                    getRootUrl()
+                            + "/team/"
+                            + Objects.requireNonNull(postResponse.getHeaders()).getFirst("teamName"),
+                    HttpMethod.GET,
+                    entity,
+                    Team.class);
 
     logger.info("Get Pokemon Team Response:" + Objects.requireNonNull(response.getBody()).toString());
 
     Assert.assertEquals(
-        Objects.requireNonNull(response.getBody()).getTeamName(), teamPost.getTeamName());
+            Objects.requireNonNull(response.getBody()).getTeamName(), teamPost.getTeamName());
 
     Assert.assertEquals(response.getBody().getFirstPokemon().getName(), teamPost.getFirstPokemon().toLowerCase());
 
     Assert.assertEquals(
-        response.getBody().getSecondPokemon().getName(), teamPost.getSecondPokemon().toLowerCase());
+            response.getBody().getSecondPokemon().getName(), teamPost.getSecondPokemon().toLowerCase());
 
     Assert.assertEquals(response.getBody().getThirdPokemon().getName(), teamPost.getThirdPokemon().toLowerCase());
 
     Assert.assertEquals(
-        response.getBody().getFourthPokemon().getName(), teamPost.getFourthPokemon().toLowerCase());
+            response.getBody().getFourthPokemon().getName(), teamPost.getFourthPokemon().toLowerCase());
 
     Assert.assertEquals(response.getBody().getFifthPokemon().getName(), teamPost.getFifthPokemon().toLowerCase());
 
@@ -239,17 +271,221 @@ public class PokemonDreamTeamTests {
 
     Assert.assertTrue(response.getStatusCode().is2xxSuccessful());
 
-    ResponseEntity<Team> deleteTeam =
+    ResponseEntity deleteTeam =
             restTemplate.exchange(
                     getRootUrl()
                             + "/team/"
                             + Objects.requireNonNull(postResponse.getHeaders()).getFirst("teamName"),
                     HttpMethod.DELETE,
                     entity,
-                    (Class<Team>) null);
+                    (Class<?>) null);
 
     logger.info("Delete Pokemon Team status code returned: " + deleteTeam.getStatusCode());
 
     Assert.assertEquals(deleteTeam.getStatusCode(), HttpStatus.NO_CONTENT);
   }
+
+  @Test
+  public void getPokemonTeamWithTeamNameThatNotExists() {
+    Faker faker = new Faker();
+    String teamName = faker.name().username();
+
+    HttpHeaders headers = new HttpHeaders();
+    HttpEntity<String> entity = new HttpEntity<>(null, headers);
+    ResponseEntity<Team> response =
+            restTemplate.exchange(
+                    getRootUrl()
+                            + "/team/"
+                            + teamName,
+                    HttpMethod.GET,
+                    entity,
+                    (Class<Team>) null);
+
+    Assert.assertEquals(response.getStatusCode(), HttpStatus.NOT_FOUND);
+  }
+
+  @Test
+  public void getPokemonTeamWithouTeamNameInThePath() {
+    HttpHeaders headers = new HttpHeaders();
+    HttpEntity<String> entity = new HttpEntity<>(null, headers);
+    ResponseEntity<Team> response =
+            restTemplate.exchange(
+                    getRootUrl()
+                            + "/team/",
+                    HttpMethod.GET,
+                    entity,
+                    (Class<Team>) null);
+
+    Assert.assertEquals(response.getStatusCode(), HttpStatus.METHOD_NOT_ALLOWED);
+  }
+
+  @Test
+  public void updateAnExistingPokemonTeam() {
+    TeamPost teamPost = createNewTeam();
+
+    ResponseEntity postResponse =
+            restTemplate.postForEntity(getRootUrl() + "/team", teamPost, null);
+
+      logger.info("Team Created");
+
+    HttpHeaders headers = new HttpHeaders();
+
+    TeamPatch teamPatch = updateTeam();
+
+    HttpEntity<TeamPatch> patchEntity = new HttpEntity<>(teamPatch, null);
+
+    ResponseEntity teamPatchRequest =
+            restTemplate.exchange(getRootUrl()
+                            + "/team/"
+                            + Objects.requireNonNull(postResponse.getHeaders()).getFirst("teamName"),
+                    HttpMethod.PATCH,
+                    patchEntity,  (Class<Team>) null );
+
+    logger.info("Team Updated");
+
+    HttpEntity<String> entity = new HttpEntity<>(null, headers);
+
+    ResponseEntity<Team> response =
+            restTemplate.exchange(
+                    getRootUrl()
+                            + "/team/"
+                            + Objects.requireNonNull(postResponse.getHeaders()).getFirst("teamName"),
+                    HttpMethod.GET,
+                    entity,
+                    Team.class);
+
+    logger.info("Get Pokemon Team Response:" + Objects.requireNonNull(response.getBody()).toString());
+
+    Assert.assertEquals(response.getBody().getFirstPokemon().getName(), teamPatch.getFirstPokemon().toLowerCase());
+
+    Assert.assertEquals(
+            response.getBody().getSecondPokemon().getName(), teamPatch.getSecondPokemon().toLowerCase());
+
+    Assert.assertEquals(response.getBody().getThirdPokemon().getName(), teamPatch.getThirdPokemon().toLowerCase());
+
+    Assert.assertEquals(
+            response.getBody().getFourthPokemon().getName(), teamPatch.getFourthPokemon().toLowerCase());
+
+    Assert.assertEquals(response.getBody().getFifthPokemon().getName(), teamPatch.getFifthPokemon().toLowerCase());
+
+    Assert.assertEquals(response.getBody().getLastPokemon().getName(), teamPatch.getLastPokemon().toLowerCase());
+
+    Assert.assertTrue(response.getStatusCode().is2xxSuccessful());
+
+    ResponseEntity deleteTeam =
+            restTemplate.exchange(
+                    getRootUrl()
+                            + "/team/"
+                            + Objects.requireNonNull(postResponse.getHeaders()).getFirst("teamName"),
+                    HttpMethod.DELETE,
+                    entity,
+                    (Class<?>) null);
+
+    logger.info("Delete Pokemon Team status code returned: " + deleteTeam.getStatusCode());
+
+    Assert.assertEquals(deleteTeam.getStatusCode(), HttpStatus.NO_CONTENT);
+  }
+
+  @Test
+  public void updatePokemonTeamWithTeamNameThatNotExists() {
+    Faker faker = new Faker();
+
+    String teamName = faker.name().username();
+
+    TeamPatch teamPatch = updateTeam();
+
+    HttpEntity<TeamPatch> patchEntity = new HttpEntity<>(teamPatch, null);
+
+    ResponseEntity teamPatchRequest =
+            restTemplate.exchange(getRootUrl()
+                            + "/team/"
+                            + teamName,
+                    HttpMethod.PATCH,
+                    patchEntity,  (Class<?>) null );
+
+    Assert.assertEquals(teamPatchRequest.getStatusCode(), HttpStatus.NOT_FOUND);
+  }
+
+  @Test
+  public void updatePokemonTeamWithouTeamNameInThePath() {
+    TeamPatch teamPatch = updateTeam();
+
+    HttpEntity<TeamPatch> patchEntity = new HttpEntity<>(teamPatch, null);
+
+    ResponseEntity teamPatchRequest =
+            restTemplate.exchange(getRootUrl()
+                            + "/team/",
+                    HttpMethod.PATCH,
+                    patchEntity,  (Class<?>) null );
+
+    Assert.assertEquals(teamPatchRequest.getStatusCode(), HttpStatus.METHOD_NOT_ALLOWED);
+  }
+
+  @Test
+  public void deleteAnExistingPokemonTeam() {
+    TeamPost teamPost = createNewTeam();
+
+    ResponseEntity postResponse =
+            restTemplate.postForEntity(getRootUrl() + "/team", teamPost, null);
+
+    HttpHeaders headers = new HttpHeaders();
+
+    HttpEntity<String> entity = new HttpEntity<>(null, headers);
+
+    logger.info("Team Created");
+
+    ResponseEntity deleteTeam =
+            restTemplate.exchange(
+                    getRootUrl()
+                            + "/team/"
+                            + Objects.requireNonNull(postResponse.getHeaders()).getFirst("teamName"),
+                    HttpMethod.DELETE,
+                    entity,
+                    (Class<?>) null);
+
+    logger.info("Delete Pokemon Team status code returned: " + deleteTeam.getStatusCode());
+
+    Assert.assertEquals(deleteTeam.getStatusCode(), HttpStatus.NO_CONTENT);
+  }
+
+  @Test
+  public void deletePokemonTeamWithTeamNameThatNotExists() {
+    Faker faker = new Faker();
+
+    String teamName = faker.name().username();
+
+    HttpHeaders headers = new HttpHeaders();
+
+    HttpEntity<String> entity = new HttpEntity<>(null, headers);
+
+    ResponseEntity deleteTeam =
+            restTemplate.exchange(
+                    getRootUrl()
+                            + "/team/"
+                            + teamName,
+                    HttpMethod.DELETE,
+                    entity,
+                    (Class<?>) null);
+
+    Assert.assertEquals(deleteTeam.getStatusCode(), HttpStatus.NOT_FOUND);
+  }
+
+  @Test
+  public void deletePokemonTeamWithoutTeamNameInThePath() {
+    HttpHeaders headers = new HttpHeaders();
+
+    HttpEntity<String> entity = new HttpEntity<>(null, headers);
+
+    ResponseEntity deleteTeam =
+            restTemplate.exchange(
+                    getRootUrl()
+                            + "/team/",
+                    HttpMethod.DELETE,
+                    entity,
+                    (Class<?>) null);
+
+    Assert.assertEquals(deleteTeam.getStatusCode(), HttpStatus.METHOD_NOT_ALLOWED);
+  }
 }
+
+
